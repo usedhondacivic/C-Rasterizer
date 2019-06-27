0aa943825d0f3c0b71f98c8a890cf8de417f0cb8
Michael Crum <micacer48@gmail.com>
2019-06-27 11:58:03 -0600

    Init

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout << "Type a number" << endl;
+    cin >> x;
+    x++;
+    cout << "Your number is: " << x;
+    return 0;
+}
