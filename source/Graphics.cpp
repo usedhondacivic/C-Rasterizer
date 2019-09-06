fdb3068ca4c717f77b235f10cacea72751df8234
usedhondacivic <micacer48@gmail.com>
2019-09-05 21:25:01 -0600

    Work on sdl2_gfx

--- source/Graphics.cpp
+++ source/Graphics.cpp
@@ -1,5 +1,4 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 
 #include "Main.h"
 #include "Engine3D.h"
@@ -10,4 +9,8 @@ void drawTriangle(triangle &tri){
     SDL_RenderDrawLine( gRenderer, (int)tri.points[0].x, (int)tri.points[0].y, (int)tri.points[1].x, (int)tri.points[1].y);
     SDL_RenderDrawLine( gRenderer, (int)tri.points[1].x, (int)tri.points[1].y, (int)tri.points[2].x, (int)tri.points[2].y);
     SDL_RenderDrawLine( gRenderer, (int)tri.points[2].x, (int)tri.points[2].y, (int)tri.points[0].x, (int)tri.points[0].y);
+    thickLineColor(gRenderer, 0, 0, constants::SCREEN_WIDTH, constants::SCREEN_HEIGHT, 20, 0xFF00FFFF) ;
+    thickLineColor(gRenderer, 0, constants::SCREEN_HEIGHT, constants::SCREEN_WIDTH, 0, 20, 0xFF00FFFF) ;
+    circleColor(gRenderer, constants::SCREEN_WIDTH/2, constants::SCREEN_HEIGHT/2, 33, 0xff00ff00);
+    filledCircleColor(gRenderer, constants::SCREEN_WIDTH/2, constants::SCREEN_HEIGHT/2, 30, 0xff00ffcc);
 }
