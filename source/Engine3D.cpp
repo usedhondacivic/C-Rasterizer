b57c28cc5e8fea90c800ce1d171d4464acb907cc
usedhondacivic <micacer48@gmail.com>
2019-09-10 13:48:30 -0600

    Work on file loading

--- source/Engine3D.cpp
+++ source/Engine3D.cpp
@@ -1,6 +1,9 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <iostream>
+#include <fstream>
+#include <strstream>
+#include <string>
 #include <vector>
 #include <math.h>
 #include <chrono>
@@ -13,6 +16,39 @@
 struct mesh
 {
     std::vector<triangle> triangles;
+
+    bool LoadFromObjectFile(std::string sFilename){
+        std::ifstream f(sFilename);
+        if(!f.is_open()){
+            return false;
+        }
+
+        std::vector<vec3d> verts;
+
+        while(!f.eof()){
+            char line[128];
+            f.getline(line, 128);
+
+            std::strstream s;
+            s << line;
+
+            char junk;
+
+            if(line[0] == 'v'){
+                vec3d v;
+                s >> junk >> v.x >> v.y >> v.z;
+                verts.push_back(v);
+            }
+
+            if(line[0] == 'f'){
+                int f[3];
+                s >> junk >> f[0] >> f[1] >> f[2];
+                triangles.push_back({ verts[f[0] - 1],  verts[f[1] - 1], verts[f[2] - 1]});
+            }
+        }
+
+        return true;
+    }
 };
 
 struct matrix4x4
