4e409c030d067bdd44a02a0563173531c1a2e257
Michael Crum <micacer48@gmail.com>
2019-09-16 09:15:00 -0600

    Work on vector class

--- source/Engine3D.h
+++ source/Engine3D.h
@@ -7,16 +7,105 @@ public:
         struct{float x, y, z; };
         struct{float pitch, yaw, roll; };
         float n[3] = {0, 0, 0};
-    }
+    };
 
     vec3d(){
-        x
+        x = y = z = 0;
+    }
+
+    vec3d(float a, float b, float c){
+        x = a; y = b; z = c;
+    }
+
+    float length() const{
+        return sqrtf(x * x + y * y + z * z);
+    }
+
+    vec3d normal(){
+        return *this / length();
+    }
+
+    float dot(const vec3d& b){
+        return acosf((x * b.x + y * b.y + z * b.z) / (length() * b.length()));
+    }
+
+    float prod(const vec3d& b){
+        return (x * b.x + y * b.y + z * b.z);
+    }
+
+    vec3d cross(const vec3d& b){
+        return {
+            n[1] * b.n[2] - n[2] * b.n[1],
+            n[2] * b.n[0] - n[0] * b.n[2],
+            n[0] * b.n[1] - n[1] * b.n[0]
+        };
+    }
+
+    vec3d& operator +=(const vec3d& rhs)
+    {
+        this->x += rhs.x;
+        this->y += rhs.y;
+        this->z += rhs.z;
+        return *this;
+    }
+
+    vec3d& operator-=(const vec3d& rhs){
+        this->x -= rhs.x;
+        this->y -= rhs.y;
+        this->z -= rhs.z;
+        return *this;
+    }
+
+    vec3d& operator*=(const float rhs){
+        this->x *= rhs;
+        this->y *= rhs;
+        this->z *= rhs;
+        return *this;
+    }
+
+    vec3d& operator/=(const float rhs){
+        this->x /= rhs;
+        this->y /= rhs;
+        this->z /= rhs;
+        return *this;
+    }
+
+    vec3d& operator+(const vec3d& rhs){
+        vec3d r;
+        r.x = this->x + rhs.x;
+        r.y = this->y + rhs.y;
+        r.z = this->z + rhs.z;
+        return r;
+    }
+
+    vec3d& operator-(const vec3d& rhs){
+        vec3d r;
+        r.x = this->x - rhs.x;
+        r.y = this->y - rhs.y;
+        r.z = this->z - rhs.z;
+        return r;
+    }
+
+    vec3d& operator*(const vec3d& rhs){
+        vec3d r;
+        r.x = this->x * rhs.x;
+        r.y = this->y * rhs.y;
+        r.z = this->z * rhs.z;
+        return r;
+    }
+
+    vec3d& operator/(const vec3d& rhs){
+        vec3d r;
+        r.x = this->x / rhs.x;
+        r.y = this->y / rhs.y;
+        r.z = this->z / rhs.z;
+        return r;
     }
 };
 
 struct triangle
 {
-    vec3d points[3] = {0};
+    vec3d points[3] = {{}};
 
     vec3d color;
 };
